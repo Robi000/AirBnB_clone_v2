4e944d85e986b3fb3f0f2b2bfe30b95fde479816
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-12 16:19:42 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    user = relationship('Place', backref='user')
+    reviews = relationship("Review", backref="user")
 
     def __init__(self, *args, **kwargs):
         """initializes Place"""
