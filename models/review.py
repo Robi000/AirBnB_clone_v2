3481362f3725714c83bba34e7904ba34c6f6bd7e
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-12 16:14:44 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
+    __tablename__ = "reviews"
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initializes Place"""
         super().__init__(*args, **kwargs)
+
