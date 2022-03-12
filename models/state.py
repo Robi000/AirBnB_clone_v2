73e6454944ff91a72735cfdd9d089474a08f400e
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-12 16:18:54 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, String, Column
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+    __table_args__ = {'extend_existing': True}
+    name = Column(String(128), nullable=False)
+    citiy = relationship("City", backref="state")
 
     def __init__(self, *args, **kwargs):
         """initializes Place"""
