e5a1adb4771578eee25bacf5052d3fa60d6df9d7
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-12 16:10:57 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ this is the aminity class"""
+    
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", overlaps="amenities", secondary="place_amenity",
+                                   viewonly=False)
 
     def __init__(self, *args, **kwargs):
         """initializes Place"""
