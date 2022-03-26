3528d44b84e4a4db4074b4f808503f3ae2ca576c
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-26 16:33:33 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
 from fabric.api import *
-"""this will pack webstatic ready for deploy 
-    """
+
 packing = __import__('1-pack_web_static').do_pack
 to_server = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['ubuntu@35.229.89.215', 'ubuntu@3.235.60.220']
@@ -10,12 +8,19 @@ fcheck = 0
 
 
 def checker():
+    """this is my small code checker
+    """
     global fcheck
     fcheck += 1
     print("check no: {}".format(fcheck))
 
 
 def deploy():
+    """this will full deploay the file to server and set it there
+
+    Returns:
+        true /false
+    """
     path = packing()
     checker()  # check no: 1  must be printed
     if path is not False:
