3c119f56b327d2c9f622f8651463b1ad8fac3fcc
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-10 21:56:51 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+""" this will show the database and give what asked """
 
 @app.route('/')
 def hello_world():
+    """ this will show the database and give what asked """
     return 'Hello HBNB!'
 
 
