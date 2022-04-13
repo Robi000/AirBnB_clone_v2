c99859b9c4848ce9f85e40d894d90562d33b7fb1
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-04-13 03:25:15 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """
 starts a Flask web application
 """
-
 
 from models import storage
 from models.state import State
@@ -11,12 +9,13 @@ from models.city import City
 from flask import Flask
 from flask import render_template
 from operator import itemgetter
+
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_state():
-    """ this will show the database and give what asked """
+    """ this will show the databas asked """
     print(storage.all(State))
 
     x = storage.all(State)
@@ -26,22 +25,16 @@ def cities_by_state():
     final = []
     for x, y in x.items():
         statex.append([y.to_dict()["id"], y.to_dict()["name"]])
-
-    print(statex)
-
-    print("\n\n\n\n")
-    print("---------------------------------")
-    print(storage.all(City)[
-          'City.521a55f4-7d82-47d9-b54c-a76916479545'].to_dict())
+    print(
+        storage.all(City)
+        ['City.521a55f4-7d82-47d9-b54c-a76916479545'].to_dict())
     # print(storage.all(City))
     for x, y in storage.all(City).items():
-        cityx.append([y.to_dict()['state_id'], y.to_dict()
-                      ["name"], y.to_dict()["id"]])
-
-    print(cityx)
+        cityx.append(
+            [y.to_dict()['state_id'],
+             y.to_dict()["name"],
+             y.to_dict()["id"]])
     meh = sorted(cityx, key=itemgetter(1))
-    print("\n\n xxxxxxxxxxxxxxxxx \n\n")
-    print(meh)
 
     for x in statex:
         state_city = []
@@ -60,39 +53,9 @@ def cities_by_state():
 
 @app.teardown_appcontext
 def session(exception):
-    """ this will show the database and give what asked """
+    """ this will show the """
     storage.close()
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-
-
-# """
-# starts a Flask web application
-# """
-
-
-# from models import storage
-# from models.state import State
-# from flask import Flask
-# from flask import render_template
-# app = Flask(__name__)
-
-
-# @app.route('/states_list', strict_slashes=False)
-# def even_or_odd():
-#     session()
-#     print(storage.all(State)[
-#           'State.421a55f4-7d82-47d9-b54c-a76916479545'].to_dict()["name"])
-    # return storage.all(State)[
-    #     'State.421a55f4-7d82-47d9-b54c-a76916479545'].to_dict()["name"]
-
-
-# @app.teardown_appcontext
-# def session(exception):
-#     storage.close()
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port='5000')
